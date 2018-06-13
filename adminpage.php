f829b52e8fe2bd161f23a5249f1c5d8a8e768241
macoveirares <macovei.rares1998@yahoo.com>
2018-06-13 17:34:28 +0300

    Approving petitions

---
+++ adminpage.php
@@ -0,0 +1,38 @@
+<?php
+	 $connect = new mysqli ('localhost', 'root','', 'OPeM');
+
+if($connect -> connect_error)
+{
+	die('connection failed bruh');
+}
+else 
+{
+}
+
+
+
+$sql="SELECT id,title,image FROM petitions where isApproved=0";
+
+
+$result = $connect->query($sql);
+
+	if($result -> num_rows> 0)
+	{
+		while($row=$result->fetch_assoc())
+		{
+			echo '<form">';
+			echo '<div class="container-petition">' ;
+			echo '<h1>';
+			echo $row['title'];
+			echo '</h1>';
+			echo '<a href="modelformular.php?id=' . $row['id'] . '"><img src="';
+			echo $row['image'];
+			echo '"></a>';
+			echo '</div>';
+			echo '</form>';
+		}
+	}
+
+
+
+?>
